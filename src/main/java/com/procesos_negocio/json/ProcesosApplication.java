28562b6ac5b04c22847d7e4c94727edb9fd94f0b
YEISON ASCANIO <ydascanioa@ufpso.edu.co>
2022-09-15 19:39:02 -0500

    commit inicial del proyecto

---
+++ src/main/java/com/procesos_negocio/json/ProcesosApplication.java
@@ -0,0 +1,13 @@
+package com.procesos_negocio.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesosApplication.class, args);
+	}
+
+}
